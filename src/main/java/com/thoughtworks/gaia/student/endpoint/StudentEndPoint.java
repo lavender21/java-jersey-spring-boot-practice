5e88b160657fcd66fa282ba1b1ae9a09337ed259
lavender21 <1573872488@qq.com>
2017-04-24 18:15:40 +0800

    Modify rest api

--- src/main/java/com/thoughtworks/gaia/student/endpoint/StudentEndPoint.java
+++ src/main/java/com/thoughtworks/gaia/student/endpoint/StudentEndPoint.java
@@ -7,15 +7,13 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Created by lavender on 17-4-24.
@@ -33,7 +31,8 @@ public class StudentEndPoint {
     @ApiOperation(value = "Get student by id", response = Student.class)
     @ApiResponses(value = {
             @ApiResponse(code = 200, message = "Get student successfully"),
-            @ApiResponse(code = 404, message = "No student matches given id")
+            @ApiResponse(code = 404, message = "No student matches given id"),
+            @ApiResponse(code = 400, message = "bad request")
     })
     @GET
     public Response getStudent(@PathParam("id") Long id) {
@@ -41,36 +40,38 @@ public class StudentEndPoint {
         return Response.ok().entity(student).build();
     }
 
-    @ApiOperation(value = "add student", response = boolean.class)
+    @ApiOperation(value = "add student", response = Student.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Add student successfully"),
-            @ApiResponse(code = 400, message = "add failed")
+            @ApiResponse(code = 201, message = "Add student successfully"),
+            @ApiResponse(code = 404, message = "not found")
     })
     @POST
-    public Response addStudent(Student student){
-        studentService.addStudent(student);
-        return Response.ok().entity(true).build();
+    public Response addStudent(Student student) throws URISyntaxException {
+        Student newstudent = studentService.addStudent(student);
+        return Response.created(new URI("/gaia/student/" + newstudent.getId())).entity(newstudent).build();
     }
 
     @Path("/{id}")
-    @ApiOperation(value = "delete student", response = boolean.class)
+    @ApiOperation(value = "delete student")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "delete student successfully"),
-            @ApiResponse(code = 400, message = "delete failed")
+            @ApiResponse(code = 204, message = "delete student successfully"),
+            @ApiResponse(code = 404, message = "not found")
     })
     @DELETE
-    public Response deleteStudent(@PathParam("id") Long id){
+    public Response deleteStudent(@PathParam("id") Long id) {
         studentService.deleteStudent(id);
-        return Response.ok().entity(true).build();
+        return Response.noContent().build();
     }
 
-    @ApiOperation(value = "add student", response = Student.class)
+    @Path("/{id}")
+    @ApiOperation(value = "update student", response = Student.class)
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Add student successfully"),
-            @ApiResponse(code = 400, message = "add failed")
+            @ApiResponse(code = 200, message = "update student successfully"),
+            @ApiResponse(code = 404, message = "not found")
     })
     @PUT
-    public Response updateStudent(Student student){
+    public Response updateStudent(@PathParam("id") Long id, Student student) {
+        student.setId(id);
         Student updateStudent = studentService.updateStudent(student);
         return Response.ok().entity(updateStudent).build();
     }
--- src/main/java/com/thoughtworks/gaia/student/service/StudentService.java
+++ src/main/java/com/thoughtworks/gaia/student/service/StudentService.java
@@ -10,46 +10,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.transaction.Transactional;
+import java.sql.SQLException;
 
 /**
  * Created by lavender on 17-4-24.
  */
 @Component
 @Transactional
-public class StudentService implements Loggable{
+public class StudentService implements Loggable {
     @Autowired
     private StudentMapper studentMapper;
 
     @Autowired
     private StudentDao studentDao;
 
-    public Student getStudent(Long id)
-    {
+    public Student getStudent(Long id) {
         StudentModel studentModel = studentDao.idEquals(id).querySingle();
-        if (studentModel == null)
-        {
+        if (studentModel == null) {
             error("student is not find with" + id);
             throw new NotFoundException();
         }
+        return studentMapper.map(studentModel, Student.class);
+    }
+
+    public Student addStudent(Student student) {
+        StudentModel studentModel = studentMapper.map(student, StudentModel.class);
+        studentDao.save(studentModel);
         return studentMapper.map(studentModel,Student.class);
     }
 
-    public void addStudent(Student student)
-    {
-        StudentModel studentModel = studentMapper.map(student,StudentModel.class);
-        studentDao.save(studentModel);
-    }
-
-    public void deleteStudent(Long id)
-    {
+    public void deleteStudent(Long id) {
         StudentModel studentModel = studentDao.idEquals(id).querySingle();
+        if (studentModel == null) {
+            error("student is not exist" + id);
+            throw new NotFoundException();
+        }
         studentDao.remove(studentModel);
     }
 
-    public Student updateStudent(Student student)
-    {
-        StudentModel studentModel = studentMapper.map(student,StudentModel.class);
+    public Student updateStudent(Student student) {
+        StudentModel studentModel = studentMapper.map(student, StudentModel.class);
         studentModel = studentDao.update(studentModel);
-        return studentMapper.map(studentModel,Student.class);
+        return studentMapper.map(studentModel, Student.class);
     }
 }
